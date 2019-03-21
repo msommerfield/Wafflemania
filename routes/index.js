47c34f1e73cd7b5baf6f4ea408eb3566ce70bf48
Mandy <mandysommerfield@gmail.com>
2019-03-21 10:56:33 -0400

    added additional json packages

--- routes/index.js
+++ routes/index.js
@@ -11,13 +11,13 @@ router.put('/:id', userController.update)
 router.delete('/:id', userController.delete)
 
 //waffle routes
-router.get('/:userId/waffles', waffleController.index)
-router.get('/:userId/waffles/new', waffleController.new)
-router.post('//:userId/waffles', waffleController.create)
-router.get('/:userId/waffles:userId', waffleController.show)
-router.get('/:userId/waffles:userId/edit', waffleController.edit)
-router.put('/:userId/waffles:userId', waffleController.update)
-router.delete('/:userId/waffles:userId', waffleController.delete)
+// router.get('/:userId/waffles', waffleController.index)
+// router.get('/:userId/waffles/new', waffleController.new)
+// router.post('//:userId/waffles', waffleController.create)
+// router.get('/:userId/waffles:userId', waffleController.show)
+// router.get('/:userId/waffles:userId/edit', waffleController.edit)
+// router.put('/:userId/waffles:userId', waffleController.update)
+// router.delete('/:userId/waffles:userId', waffleController.delete)
 
 
 module.exports = router
