c682337e4481bf278f45b66c867e9813f3e00427
Mandy <mandysommerfield@gmail.com>
2019-03-22 12:16:19 -0400

    add components and connected App.js page

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Waffles from './components/Waffles.jsx'
+import UserLogin from './components/UserLogin.jsx'
+import NavigationBar from './components/NavigationBar.jsx'
+import User from './components/User.jsx'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Router>
+          <div>
+            <div>Hola Peter is reading this</div>
+            <div>
+              <NavigationBar/>
+            </div>
+            <Switch>
+              <Route exact path="/" component={UserLogin} />
+              <Route exact path="/:userId" component={User} />
+              <Route exact path="/:userId/waffles" component={Waffles} />
+            </Switch>
+          </div>
+        </Router>
       </div>
-    );
+    )
   }
 }
 
